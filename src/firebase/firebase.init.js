6ccc0c48bd01bb02852da7c7f549d04a0887541b
Marjan-001 <marjan199721@gmail.com>
2023-04-08 20:55:22 +0600

    env local

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCWHal0JjdWXMccQIsZUTXcVHr_m00GtgI",
-    authDomain: "homecook-ca296.firebaseapp.com",
-    projectId: "homecook-ca296",
-    storageBucket: "homecook-ca296.appspot.com",
-    messagingSenderId: "926923767225",
-    appId: "1:926923767225:web:3ad1617c92a7d50adb8a68"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
